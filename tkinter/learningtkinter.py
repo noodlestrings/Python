867ea2b8cd8564a3b7841d403c8d46ae454353e9
Sam Sidcup <sammystradling2006@gmail.com>
2022-03-18 19:47:42 +0000

    added learningtkinter.py

--- tkinter/learningtkinter.py
+++ tkinter/learningtkinter.py
@@ -1,22 +1,46 @@
-# from tkinter import *
+import this
+import tkinter as tk
 
-# window = Tk()
+window = tk.Tk()
+window.title("Hello World")
 
-# labelWidget = Label(window, text="Hello world")
-# labelWidgetGrid = Label(window, text="Amongus")
+# title with no args returns the title
+titleOfWindow = window.title()
+
+window_width = 550
+window_height = 500
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+center_x = (screen_width // 2) - (window_width // 2)
+center_y = (screen_height // 2) - (window_height // 2)
+
+# geometry method takes 'widthxheight+-X+-Y'
+window.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+# stops the window being able to be resized  resizable(width, height)
+window.resizable(False, True)
+
+# .minsize or .maxsize takes the width and height that can be resized to
+window.minsize(500, 100)
+window.maxsize(500, 800)
 
 
-# labelWidgetGrid.grid(row=0, column=0)
-# labelWidget.grid(
-#     row=1, column=0
-# )  # pack() adds the widget to the window, can't use grid and pack at same time
+# 0 fully transparent, 1 fully opaque
+# THIS ONLY FOR LINUX
+window.wait_visibility(window)
+# ONLY THIS WORKS ON WINDOWS
+window.attributes('-alpha', 0.5)
 
-# window.mainloop()
+# shows window above all others
+window.attributes("-topmost", 1)
 
-amongus = 5
-match amongus:
-    case 1:
-        print(1)
-    case 5:
-        print(5)
+
+# label creates a widget and takes two arguments, the container and options (e.g. text)
+message = tk.Label(window, text="Hello World")
+message.pack()
+
+
+# keeps the window displaying, should be after all other code
+window.mainloop()
 
---
+++ tkinter/tut link.md
@@ -0,0 +1,1 @@
+# https://www.pythont# geometry method takes 'widthxheight+-X+-Y'window.geometry("500x500-50-75")utorial.net/tkinter/tkinter-window/
