b928e30f5c2562a3e4e6488e39caf8d01e428805
Sam Sidcup <sammystradling2006@gmail.com>
2022-05-01 21:31:00 +0100

    finished backspace-string-compare.py

--- misc/backspace-string-compare.py
+++ misc/backspace-string-compare.py
@@ -1,22 +1,25 @@
-s = "ab#c"
-t = "ad#c"
+s = "y#fo##f"
+t = "y#f#o##f"
+
 
 sarr = []
 tarr = []
 for i in range(len(s)):
     sarr.append(s[i])
 for j in range(len(t)):
-    tarr.append(s[j])
+    tarr.append(t[j])
 
 while "#" in sarr:
     ind = sarr.index("#")
-    sarr.pop(ind - 1)
-    sarr.pop(ind - 1) #index changes after list becomes shorter
+    sarr.pop(ind)
+    if (ind - 1) >= 0:
+        sarr.pop(ind - 1)  # index changes after list becomes shorter
 
 while "#" in tarr:
     ind = tarr.index("#")
-    tarr.pop(ind - 1)
-    tarr.pop(ind - 1)
+    tarr.pop(ind)
+    if (ind - 1) >= 0:
+        tarr.pop(ind - 1)
 
-if tarr == sarr :
-    return true
+if tarr == sarr:
+    print("true")
