a1fd4b383b35f9399b87f5af0b7422d55806441b
Sam Lee <sammystradling2006@gmail.com>
2022-05-24 16:03:59 +0100

    testing complete, all working

--- csv/csvLoginSystem/csvhandling.py
+++ csv/csvLoginSystem/csvhandling.py
@@ -1,27 +1,50 @@
 import csv
 
+from click import password_option
+
+
+def remove_user_input_validation(username):
+    passwordQuery = input(
+        f"Please enter the password for account name {username}: ")
+    UsrPswdExists = False
+    with open(r'csv\csvLoginSystem\users.csv', 'r') as file:
+        csv_reader = csv.DictReader(file, delimiter=";")
+        for line in csv_reader:
+            if line['username'] == username and line['password'] == passwordQuery:
+                UsrPswdExists = True
+                break
+    if UsrPswdExists != True:
+        print("\nSorry, you entered the password and/or account name incorrectly")
+        return "Invalid"
+
+    # if exists:
+    #     passwordQuery = input(f"Please enter the password for account name {username}: ")
+    #     with open(r'csv\csvLoginSystem\users.csv', 'r+') as file:
+    #         for line in csv_reader:
+    #             if line[]
+
 
 def check_user():
     with open(r'csv\csvLoginSystem\users.csv', 'r') as file:
 
-        username = input("Please enter your username: ")
-        password = input("Please enter your password: ")
+        username = input("\nPlease enter your username: ")
+        password = input("\nPlease enter your password: ")
 
         csv_reader = csv.DictReader(file, delimiter=";")
         usernameExists = False
         for line in csv_reader:
             if (line['username'] == username) and (line['password'] == password):
                 usernameExists = True
-                print("You have successfully logged in to your account")
+                print("\nYou have successfully logged in to your account")
                 exit()
             elif (line['username'] == username) and (line['password'] != password):
                 usernameExists = True
-                print("You have entered the password incorrectly, please try again")
+                print("\nYou have entered the password incorrectly, please try again")
                 check_user()
-            
+
         if usernameExists == False:
             createAccount = input(
-                "Sorry, that account doesn't seem to exist.\nEnter 'a' to create a new account or 'r' to retry: ")
+                "\nSorry, that account doesn't seem to exist.\nEnter 'a' to create a new account or 'r' to retry: ")
             if createAccount.lower() == 'a':
                 add_user()
                 exit()
@@ -34,16 +57,16 @@ def add_user():
 
         csv_reader = csv.DictReader(file, delimiter=";")
 
-        firstName = input("Enter your first name: ")
-        lastName = input("Enter your last name: ")
-        userName = input("Enter your userName: ")
-        email = input("Enter your email: ")
-        password = input("Enter your password: ")
+        firstName = input("\nEnter your first name: ")
+        lastName = input("\nEnter your last name: ")
+        userName = input("\nEnter your userName: ")
+        email = input("\nEnter your email: ")
+        password = input("\nEnter your password: ")
 
         for line in csv_reader:
             if line['username'] == userName:
                 loginQuery = input(
-                    f"An account with the username {userName} already exists, enter 'L' to login or 'Q' to quit: ")
+                    f"\nAn account with the username {userName} already exists, enter 'L' to login or 'Q' to quit: ")
                 if loginQuery.lower() == "l":
                     check_user()
                 elif loginQuery.lower() == "q":
@@ -56,11 +79,28 @@ def add_user():
         csv_writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=";")
         csv_writer.writerow({'first_name': firstName, 'last_name': lastName,
                             'username': userName, 'email': email, 'password': password})
-        print("Account successfully created")
+        print("\nAccount successfully created")
 
 
 def remove_user():
-    pass
+    toBeDeleted = input(
+        "\nPlease enter the username of the account to be removed: ")
 
+    if remove_user_input_validation(toBeDeleted) == "Invalid":
+        exit()
 
-check_user()
+    csvContent = []
+    with open(r'csv\csvLoginSystem\users.csv', 'r+') as file:
+        csv_reader = csv.DictReader(file, delimiter=";")
+        for line in csv_reader:
+            if line['username'] != toBeDeleted:
+                csvContent.append(line)
+
+    with open(r'csv\csvLoginSystem\users.csv', 'w', newline="") as fileBackedUp:
+        csv_writer = csv.DictWriter(fileBackedUp, fieldnames=['first_name', 'last_name',
+                                                              'username', 'email', 'password'], delimiter=";")
+        csv_writer.writeheader()
+        for line in csvContent:
+            csv_writer.writerow(line)
+
+        print("\nDatabase updated")
--- csv/csvLoginSystem/main.py
+++ csv/csvLoginSystem/main.py
@@ -1,18 +1,23 @@
 from csvhandling import check_user, add_user, remove_user
 
 
+validChoice = False
+while validChoice == False:
 
+    openingChoice = input(
+        "Welcome to the CSV login system.\n\nTo log in to an account, press 'L'.\nTo add an account, press 'A'.\nTo remove an account, press 'R': ")
 
-openingChoice = input(
-    "Welcome to the CSV login system.\nTo log in to an account, press 'L'.\nTo add an account, press 'A'.\nTo remove an account, press 'R': ")
+    if openingChoice.lower() == 'l':  # login
+        check_user()
+        validChoice = True
 
+    elif openingChoice.lower() == 'a':  # add account
+        add_user()
+        validChoice = True
 
-if openingChoice.lower() == 'l':  # login
+    elif openingChoice.lower() == 'r':  # remove
+        remove_user()
+        validChoice = True
 
-    check_user()
-
-elif openingChoice.lower() == 'a':  # add account
-    add_user()
-
-elif openingChoice.lower() == 'r': #remove
-    remove_user() #unifnished
+    else:
+        print("Sorry, that was not an option available. Please try again.\n\n")
